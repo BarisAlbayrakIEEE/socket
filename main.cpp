b39b734fef941868068b6f108290764983b6c5e8
BarisAlbayrakIEEE <baris.albayrak.ieee@gmail.com>
2025-11-13 21:28:56 +0300

    uniform

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "print_addrs.h"
+
+int main(void) {
+    return print_addrs();
+}
--- print_addrs.h
+++ print_addrs.h
@@ -1,12 +1,23 @@
+#ifndef PRINT_ADDRS_H
+#define PRINT_ADDRS_H
+
 #include "get_uniform_addrs.h"
 
 using namespace ba_socket;
 
-int main(void) {
+int print_addrs(void) {
     SOCKET_STARTUP();
 
+    // get the addrs
     Uniform_Addr uniform_addrs[100];
     size_t count = get_uniform_addrs(uniform_addrs, 100);
+    if (!count) {
+        printf("No addresses found.\n");
+        SOCKET_CLEANUP();
+        return 1;
+    }
+
+    // print the addrs
     for (size_t i = 0; i < count; ++i) {
         char ip[100];
         getnameinfo(
@@ -23,3 +34,5 @@ int main(void) {
     SOCKET_CLEANUP();
     return 0;
 }
+
+#endif // PRINT_ADDRS_H
