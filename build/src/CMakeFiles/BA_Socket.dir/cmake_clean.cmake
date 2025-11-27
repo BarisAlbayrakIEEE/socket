91354bd43f8e83e867cc1a6fae84f0dcdf5bbf45
BarisAlbayrakIEEE <baris.albayrak.ieee@gmail.com>
2025-11-28 00:52:15 +0300

    thread pool

---
+++ build/src/CMakeFiles/BA_Socket.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../bin/BA_Socket"
+  "../bin/BA_Socket.pdb"
+  "CMakeFiles/BA_Socket.dir/TCP_client__select__MT.cpp.o"
+  "CMakeFiles/BA_Socket.dir/TCP_client__select__MT.cpp.o.d"
+  "CMakeFiles/BA_Socket.dir/TCP_client__select__ST.cpp.o"
+  "CMakeFiles/BA_Socket.dir/TCP_client__select__ST.cpp.o.d"
+  "CMakeFiles/BA_Socket.dir/TCP_server__select__MT.cpp.o"
+  "CMakeFiles/BA_Socket.dir/TCP_server__select__MT.cpp.o.d"
+  "CMakeFiles/BA_Socket.dir/TCP_server__select__ST.cpp.o"
+  "CMakeFiles/BA_Socket.dir/TCP_server__select__ST.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BA_Socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
